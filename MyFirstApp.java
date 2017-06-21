d708a13a57622202ae4215f0a9dc1d6e632b8a04
Paul Anand <paul.anand@mcruncher.com>
2017-06-21 16:57:42 +0800

    Added beer song

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,6 +1,11 @@
 public class MyFirstApp {
 public static void main (String[] args) {
-        System.out.println(“I Rule !”);
-        System.out.println(“The World”);
+        System.out.println("I Rule !");
+        System.out.println("The World");
+        int x = 3;
+        String name = "Dirk";
+        x = x * 17;
+        System.out.println("x is " + x);
+        double d = Math.random();
 }
 }
